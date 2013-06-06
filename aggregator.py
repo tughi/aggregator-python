ef09701835599d68bff629fdd5e1b1ac4837623a
tughi <admin@tughi.com>
2013-06-06 15:14:05 +0200

    list feeds and delete feed API calls return 4xx errors for API calls log storm statements

--- aggregator.py
+++ aggregator.py
@@ -1,6 +1,6 @@
-from bottle import Bottle, request
+from bottle import Bottle, request, response
 from storm.store import Store
-from persistence import open_database, Feed
+from persistence import open_database, Feed, Entry
 
 import feedparser
 
@@ -9,38 +9,36 @@ app = Bottle()
 app.database = open_database()
 
 
-def success(content=None):
-    """
-    Creates a success response.
+if True:
+    # debug mode
 
-    @param content: optional content to be bundled with the response
-    @return: a response dict
-    """
+    import storm.tracer
 
-    return {
-        'result': 'success',
-        'content': content
-    }
+    class PrintStatementTracer(object):
+        def connection_raw_execute(self, connection, raw_cursor, statement, params):
+            if params:
+                for param in params:
+                    statement = statement.replace('?', repr(param.get()), 1)
+            print('STORM: %s' % statement)
+
+    storm.tracer.install_tracer(PrintStatementTracer())
 
 
-def error(message, content=None):
-    """
-    Creates an error response.
+@app.get('/feeds')
+def feeds():
+    result = []
 
-    @param message: error message
-    @param content: optional content to be bundled with the response
-    @return: a response dict
-    """
+    store = Store(app.database)
 
-    response = {
-        'result': 'error',
-        'message': message
-    }
+    feeds = store.find(Feed)
+    for feed in feeds:
+        result.append({
+            'id': feed.id,
+            'title': feed.title,
+            'url': feed.url
+        })
 
-    if content:
-        response['content'] = content
-
-    return response
+    return result
 
 
 @app.post('/feeds')
@@ -49,7 +47,8 @@ def new_feed():
     title = request.forms.get('title')
 
     if not url:
-        return error(-1, 'missing url parameter')
+        response.status = 400
+        return 'missing url parameter'
 
     store = Store(app.database)
 
@@ -57,22 +56,44 @@ def new_feed():
     feed = store.find(Feed, url=url).one()
 
     if feed:
-        return error('feed already exists', {'title': feed.title})
+        response.status = 409
+        return 'a feed already exists with this url: %d' % feed.id
 
     data = feedparser.parse(url)
 
     if not data:
-        return error('failed to load the feed')
+        response.status = 400
+        return 'failed to load the feed'
 
-    if not title:
-        title = data.feed.title
-
-    feed = store.add(Feed(url=url, title=unicode(title), etag=data.get('etag'), modified=data.get('modified')))
+    feed = {
+        'url': url,
+        'title': unicode(title or data.feed.title),
+        'etag': str(data.etag) if 'etag' in data else None,
+        'modified': str(data.modified) if 'modified' in data else None,
+    }
+    feed = store.add(Feed(**feed))
 
     # TODO: store feed entries too
 
     store.commit()
 
-    return success({
+    return {
+        'id': feed.id,
         'title': feed.title
-    })
+    }
+
+
+@app.delete('/feeds/<id:int>')
+def delete_feed(id):
+    store = Store(app.database)
+    try:
+        store.find(Entry, feed_id=id).remove()
+        store.find(Feed, id=id).remove()
+    except RuntimeError:
+        store.rollback()
+
+        response.status = 409
+        return e.args
+    else:
+        store.commit()
+    return
