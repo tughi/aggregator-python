ca532aefe626c9ae4a58ca1d2a75818a9c26b7b1
Tughi <admin@tughi.com>
2013-07-16 20:10:23 +0200

    load more entries on click

--- reader/js/aggregator.js
+++ reader/js/aggregator.js
@@ -9,7 +9,12 @@ $(function () {
     });
 
     var Session = Backbone.Model.extend({
-        url: "/reader/session"
+        url: "/reader/session",
+
+        hasEntries: function () {
+            var entries = this.get("entries");
+            return entries && entries.length;
+        }
     });
 
     var EntriesView = Backbone.View.extend({
@@ -18,26 +23,54 @@ $(function () {
         entries: new Entries,
 
         initialize: function () {
-            this.$entryTemplate = this.$("> #entry-template").removeAttr("id").remove();
+            var view = this;
 
-            this.listenTo(this.session, "change", this.refresh);
-            this.listenTo(this.entries, "add", this.render);
+            view.$entryTemplate = this.$("> #entry-template").removeAttr("id").remove();
 
-            this.session.fetch();
+            view.$loader = view.$("> #loader").click(function () {
+                view.fetchNextPage();
+            });
+
+            view.listenTo(this.session, "change", view.refresh);
+            view.listenTo(this.entries, "add", view.render);
+
+            view.session.fetch();
         },
 
         refresh: function () {
-            var entries = this.entries;
-            entries.reset();
+            this.currentPage = -1;
+            this.entries.reset();
 
             this.now = moment();
 
-            var page = new Entries;
-            page.fetch({data: {ids: this.session.get("entries").slice(0, 50).join(",")}})
+            if (this.session.hasEntries()) {
+                // fetch first page
+                this.fetchNextPage();
+            }
+        },
+
+        fetchNextPage: function () {
+            var PAGE_ENTRIES = 50;
+
+            var view = this;
+            var page = view.currentPage + 1;
+
+            var entryIds = view.session.get("entries");
+
+            var pageEntries = new Entries;
+            var pageEntryIds = entryIds.slice(page * PAGE_ENTRIES, Math.min((page + 1) * PAGE_ENTRIES, entryIds.length));
+            pageEntries.fetch({data: {ids: pageEntryIds.join(",")}})
                 .done(function () {
-                    page.each(function (entry) {
-                        entries.add(entry);
+                    pageEntries.each(function (entry) {
+                        view.entries.add(entry);
                     });
+
+                    view.currentPage = page;
+
+                    if ((page + 1) * PAGE_ENTRIES > entryIds.length) {
+                        // no more entries to load
+                        view.$loader.hide();
+                    }
                 });
         },
 
@@ -48,10 +81,10 @@ $(function () {
             $entry.find("#favicon").attr("href", entry.get("link")).css("background-image", "url('" + "/favicon.ico" + "')");
 
             var date = moment(entry.get("updated"));
-            var dateFormat = this.now.year() != date.year() ? "MMM DD YYYY" : this.now.date() != date.date() ? "MMM DD" : "hh:mm a";
+            var dateFormat = this.now.year() != date.year() ? "MMM DD YYYY" : this.now.date() == date.date() && this.now.month() == date.month() ? "hh:mm a" : "MMM DD";
             $entry.find("#date").text(date.format(dateFormat));
 
-            this.$el.append($entry);
+            this.$loader.before($entry);
         }
     });
 
