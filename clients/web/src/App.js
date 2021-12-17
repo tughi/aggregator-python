cc799009f6bcc12be35b399e5f4c75813aed0cf7
Tughi <one@tughi.com>
2021-12-17 10:57:05 +0100

    Use touch events to navigate between entries

--- clients/web/src/App.js
+++ clients/web/src/App.js
@@ -1,8 +1,9 @@
+import classNames from "classnames"
 import { BrowserRouter as Router, Switch, Redirect, Route } from "react-router-dom"
 import { Reader } from "./reader/Reader"
 
 export const App = () => (
-   <div className="App">
+   <div className={classNames("App", navigator.maxTouchPoints > 0 ? "touch-mode" : "click-mode")}>
       <Router>
          <Switch>
             <Route path="/reader" component={Reader} />
--- clients/web/src/reader/EntryViewer.js
+++ clients/web/src/reader/EntryViewer.js
@@ -20,8 +20,125 @@ export const EntryViewer = () => {
       }
    }, [isViewerVisible, activeEntry, toggleEntryReadState])
 
+   const ref = useRef(null)
+   useEffect(() => {
+      const viewer = ref.current
+
+      if (!viewer.closest('.App.touch-mode')) {
+         return
+      }
+
+      const body = viewer.querySelector('.body')
+
+      const TOUCH_STATE_DETECT = 1
+      const TOUCH_STATE_DRAGGING = 1
+
+      let touchState = null
+      let touchDraggingDirection = 0
+      let touchStartX = 0
+      let touchStartY = 0
+      let touchDeltaX = 0
+      let touchDeltaY = 0
+
+      let prevEntry = null
+      let currEntry = null
+      let nextEntry = null
+
+      const touchStartListener = (event) => {
+         prevEntry = body.querySelector('.entry.prev-1')
+         currEntry = body.querySelector('.entry.active')
+         nextEntry = body.querySelector('.entry.next-1')
+
+         // TODO: ignore touch events if the touch target is inside of a scrollable container
+
+         if (prevEntry || nextEntry) {
+            touchState = TOUCH_STATE_DETECT
+            touchStartX = event.touches[0].clientX
+            touchStartY = event.touches[0].clientY
+            touchDraggingDirection = 0
+         } else {
+            touchState = null
+         }
+      }
+
+      const touchMoveListener = (event) => {
+         const newTouchDeltaX = touchStartX - event.touches[0].clientX
+         touchDraggingDirection = newTouchDeltaX - touchDeltaX
+         touchDeltaX = newTouchDeltaX
+         touchDeltaY = touchStartY - event.touches[0].clientY
+
+         if (touchState === TOUCH_STATE_DETECT) {
+            let touchAbsoluteDeltaX = Math.abs(touchDeltaX)
+            let touchAbsoluteDeltaY = Math.abs(touchDeltaY)
+
+            if (touchAbsoluteDeltaX < 5 && touchAbsoluteDeltaY < 5) {
+               console.log('Movement too small for detection!')
+            } else if (touchAbsoluteDeltaX > touchAbsoluteDeltaY) {
+               touchState = TOUCH_STATE_DRAGGING
+            } else {
+               touchState = null
+            }
+         }
+
+         if (touchState === TOUCH_STATE_DRAGGING) {
+            event.preventDefault()
+
+            if (touchDeltaX > 0) {
+               if (nextEntry) {
+                  nextEntry.style.left = `calc(100% + 1rem - ${touchDeltaX}px)`
+                  nextEntry.style.transition = 'none'
+               }
+
+               currEntry.style.left = null
+               currEntry.style.transition = null
+            } else {
+               if (prevEntry) {
+                  currEntry.style.left = `${-touchDeltaX}px`
+                  currEntry.style.transition = 'none'
+               }
+
+               if (nextEntry) {
+                  nextEntry.style.left = null
+                  nextEntry.style.transition = null
+               }
+            }
+         }
+      }
+
+      const touchEndListener = (event) => {
+         if (touchState === TOUCH_STATE_DRAGGING) {
+            if (touchDeltaX > 0 && touchDraggingDirection > 0) {
+               setActiveEntryIndex(activeEntryIndex => Math.min(activeEntryIndex + 1, entriesLength - 1))
+            } else if (touchDeltaX < 0 && touchDraggingDirection < 0) {
+               setActiveEntryIndex(activeEntryIndex => Math.max(activeEntryIndex - 1, 0))
+            }
+
+            if (prevEntry) {
+               currEntry.style.left = null
+               currEntry.style.transition = null
+            }
+            if (nextEntry) {
+               nextEntry.style.left = null
+               nextEntry.style.transition = null
+            }
+         }
+      }
+
+      body.addEventListener('touchstart', touchStartListener)
+      body.addEventListener('touchmove', touchMoveListener)
+      body.addEventListener('touchend', touchEndListener)
+
+      return () => {
+         body.removeEventListener('touchstart', touchStartListener)
+         body.removeEventListener('touchmove', touchMoveListener)
+         body.removeEventListener('touchend', touchEndListener)
+
+         console.log('DUCK!')
+      }
+   }, [entriesLength, setActiveEntryIndex])
+
    return (
-      <div className={classNames("EntryViewer", "content", { active: isViewerVisible })}>
+      <div ref={ref} className={classNames("EntryViewer", "content", { active: isViewerVisible })}>
          <div className="header">
             <ActionBar>
                <ActionBar.Action icon="close" onClick={() => setIsViewerVisible(false)} />
@@ -46,6 +163,8 @@ export const EntryViewer = () => {
                   className = `prev-${activeEntryIndex - entryIndex}`
                } else if (activeEntryIndex < entryIndex) {
                   className = `next-${entryIndex - activeEntryIndex}`
+               } else {
+                  className = 'active'
                }
                return (
                   <Entry key={entry.id} className={className} entry={entry} feed={feedsById[entry.feedId]} />
